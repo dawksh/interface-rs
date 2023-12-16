84d97f31e068bf929ef059882e57dd2cfc1f63c3
Daksh <akshd@outlook.com>
2023-12-17 00:42:23 +0530

    ⚡️ chore: init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
